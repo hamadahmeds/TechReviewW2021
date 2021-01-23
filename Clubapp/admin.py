1e0f74bb40c705871f1a3402069436c916a48533
Ahmed hamad <ahemdhamads@gmail.com>
2021-01-23 15:20:21 -0800

    assignment4 commit

--- Clubapp/admin.py
+++ Clubapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Meeting, Meeting_Minutes, Resource ,Event
+
 # Register your models here.
+admin.site.register(Meeting)
+admin.site.register(Meeting_Minutes)
+admin.site.register(Resource)
+admin.site.register(Event)
--- Clubapp/models.py
+++ Clubapp/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Meeting(models.Model):
+    MeetingTittle=models.CharField(max_length=255)
+    MeetingDate=models.DateField()
+    MeetingTime=models.TimeField()
+    Location=models.CharField(max_length=255)
+    Agenda=models.TextField()
+
+    def __str__(self):
+        return self.MeetingTittle
+    
+    class Meta:
+        db_table='meeting'
+        verbose_name_plural='meetings'
+
+
+class Meeting_Minutes(models.Model):
+    MeetingID=models.ForeignKey(Meeting, on_delete=models.DO_NOTHING)
+    Attendance=models.ManyToManyField(User)
+    Minutes=models.TextField()
+
+    def __str__(self):
+        return self.MeetingID
+    
+    class Meta:
+        db_table='meeting minutes'
+        verbose_name_plural=''
+
+
+
+
+
+
+
+class Resource (models.Model):
+    ResourceName=models.CharField(max_length=255)
+    ResourceType=models.CharField(max_length=255)
+    URL=models.URLField()
+    DateEntered=models.DateField()
+    UserID=models.ForeignKey(User,on_delete=models.DO_NOTHING)
+    Description=models.TextField()
+
+
+
+
+    def __str__(self):
+        return self.ResourceName
+    
+    class Meta:
+        db_table='resources'
+
+
+class Event (models.Model):
+    EventTitle= models.CharField(max_length=255)
+    location=models.CharField(max_length=255)
+    Date=models.DateField()
+    Time=models.TimeField()
+    Description=models.TextField()
+    UserID=models.ForeignKey(User,on_delete=models.DO_NOTHING)
+    
+
+    def __str__(self): 
+        return self.EventTitle
+    class Meta:
+         db_table='event'
+
+
