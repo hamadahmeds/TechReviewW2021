6ef667c40e13b79cabfa2ed85ec8dd5ce96e7d05
Ahmed hamad <ahemdhamads@gmail.com>
2021-02-24 17:12:56 -0800

    Commit assignment8

--- Clubapp/tests.py
+++ Clubapp/tests.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
 from .models import Meeting, Meeting_Minutes, Event, Resource
+import datetime
+# import .forms Meeting
+
+
+
+
+
 class MeetingTest(TestCase):
     def setUp(self):
         self.type=Meeting(MeetingTittle='python-learning')
@@ -17,3 +24,15 @@ class ResourceTest(TestCase):
         self.type=Resource(ResourceName='meeting minutes')
     def test_string(self):
          self.assertEqual(str(self.type), 'meeting minutes')
+
+#Form tests IS NOT WORKING FOR ME , WILL HAVE TO TRY IT AGIN 
+class MeetingType_Form_Test(TestCase):
+    def test_typeform_is_valid(self):
+        data={
+            {'meetingtittle': "meeting2",
+            'meetingdate' : "2021-02-24",
+            'meetinglocation' : "seattle wa"
+            }
+         }
+    from=MeetingForm (date)
+    self.assertTrue(form.is_valid)
--- PythonClub/urls.py
+++ PythonClub/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Clubapp/',include('Clubapp.urls')),
+
+    path('accounts/', include('django.contrib.auth.urls')),
+    
 ]
